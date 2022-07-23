9e0311cdf536ef66cf232b8107925dd1be425a19
edmundchen9 <edchen7321@gmail.com>
2022-07-23 15:08:57 -0400

    syntax change

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.querySelector('.header__moon').onclick = function() {
+document.querySelector('.header_moon').onclick = function() {
     document.querySelector('body').classList.toggle('is-black');
     
 }
 
-document.querySelector('.header__mob-menu').onclick = function() {
+document.querySelector('.header_mob-menu').onclick = function() {
     document.querySelector('.header-menu').classList.toggle('is-active');
 }
