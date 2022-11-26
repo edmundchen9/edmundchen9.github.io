ee6e37807c1d81cc261cb683bc13d321f6e1c136
EdChen9 <ec3ukf@virginia.edu>
2022-11-25 20:22:32 -0500

    Nightmode actually sticks

--- js/script.js
+++ js/script.js
@@ -1,50 +1,33 @@
-document.querySelector('.header_moon').onclick = function() {
-    document.querySelector('body').classList.toggle('is-black');
-}
+let darkMode = localStorage.getItem('darkMode');
+let darkToggle = document.querySelector('.header_moon');
+let bodyToggle = document.querySelector('body');
 
+
+  if (darkMode && darkMode === '1') {
+      bodyToggle.classList.add('is-black');
+      darkToggle.classList.add('is-active');
+  }
+
+    darkToggle.onclick = function() {
+    bodyToggle.classList.toggle('is-black');
+    darkToggle.classList.toggle('is-active');
+    if (darkMode) {
+          if (darkMode === '1') {
+              localStorage.setItem('darkMode', '0');
+          } else {
+              localStorage.setItem('darkMode', '1');
+          }
+      } else {
+          localStorage.setItem('darkMode', '1');
+      }
+};
+
+
+console.log(localStorage.getItem(darkMode));
+console.log(1+4);
+
+
+// hamburger dropdown when compressed
 document.querySelector('.header_mob-menu').onclick = function() {
     document.querySelector('.header-menu').classList.toggle('is-active');
 }
-
-function toggleDark() {
-    const body = document.querySelector('body');
-    if (body.classList.contains('is-black')) {
-      body.classList.remove('is-black');
-    } else {
-      body.classList.add('is-black');
-    }
-  }
-
-
-// if (localStorage.getItem('is-black') === 'true') {
-//     document.getElementById('main-page').classList.add('is-black');
-// } 
-// // dsadafdasfds
-// const toggleBtn = document.getElementById("toggle-btn");
-// const theme = document.getElementById("theme");
-// let darkMode = localStorage.getItem("is-black");
-
-// const enableDarkMode = () => {
-//   theme.classList.add("is-black-theme");
-//   toggleBtn.classList.remove("is-black-toggle");
-//   localStorage.setItem("is-black", "enabled");
-// };
-
-// const disableDarkMode = () => {
-//   theme.classList.remove("is-black-theme");
-//   toggleBtn.classList.add("is-black-toggle");
-//   localStorage.setItem("is-black", "disabled");
-// };
-
-// if (darkMode === "enabled") {
-//   enableDarkMode(); // set state of darkMode on page load
-// }
-
-// toggleBtn.addEventListener("click", (e) => {
-//   darkMode = localStorage.getItem("is-black"); // update darkMode when clicked
-//   if (darkMode === "disabled") {
-//     enableDarkMode();
-//   } else {
-//     disableDarkMode();
-//   }
-// });
